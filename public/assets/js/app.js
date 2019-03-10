80eab0963afccfc8184b736ab10b15b2e01a46dc
Mvcampbell3 <mvcampbell3@gmail.com>
2019-03-10 18:08:19 -0400

    different animations

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,22 +1,23 @@
 $(function () {
 
-    
-    $(".area").on("click", function() {
-        $(".backBox").addClass("shadowFlip")
-        setTimeout(function(){
-            // $(".back").addClass("fall")
-            seperately("back");
-            console.log("ran");
-        }, 4250)
-        setTimeout(function(){
-            seperately("front");
-        }, 4550);
-    })
+
+    // $(".area").on("click", function () {
+    $(".back").addClass("fall")
+    console.log("ran");
+    setTimeout(function () {
+        document.getElementById("first").className = "letter back fall transition-1 lastC";
+        document.getElementById("second").className = "letter back fall transition-1 lastA";
+        document.getElementById("third").className = "letter back fall transition-1 lastM";
+        document.getElementById("fourth").className = "letter back fall transition-1 lastP";
+        document.getElementById("fifth").className = "letter back fall transition-1 lastB";
+        document.getElementById("last").className = "letter back fall transition-1 lastL";
+    }, 450);
+    // })
 
 });
 
 function seperately(name) {
-    var letters = [].slice.call(document.querySelectorAll("."+name))
+    var letters = [].slice.call(document.querySelectorAll("." + name))
     var timer = setInterval(frame, 100);
     var pos = 0;
     function frame() {
