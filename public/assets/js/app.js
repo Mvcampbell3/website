3f76025e2e25dcbb98220be649b34081d16daa18
Mvcampbell3 <mvcampbell3@gmail.com>
2019-03-19 00:21:08 -0400

    added some responsiveness

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -25,46 +25,20 @@ $(function () {
                 500
             );
         }
+        $(".navBar a").removeClass("there");
+        $(this).addClass("there");
     });
 
-    const nameArray = [
-        "Rock Paper Scissors",
-        "Yahtzee",
-        "Stardew Match Maker",
-        "Burger App",
-        "Bamazon"
-    ]
 
-    const informationArray = [
-        "This is the classic Rock Paper Scissors Game. This is a multiplay game which utilizes the firebase authentication and database technologies. Simply create a user and join in on the 2 player fun.",
-        "This is an ongoing personal project which has its beginnings back when I just started to learn web design. It started as a simple way to learn arrays and is turning into a fully finished game.",
-        "This express app is based on the video game Stardew Valley. You take a survey to see which player from the game you should marry. Since there is online multiplayer, you will also be tested against people who have already taken it. Good Luck!",
-        "This is an express app that utilizes both express-handlebars and MySql to store burgers into a menu. You can add burgers, devour them, put them back, or delete them. Sound tasty?",
-        "Bamazon is a command line node.js app that simulates the interaction on online shopping. Customers can purchase items, Managers can purchase more inventory or add new products. Supervisors can view the financial standings of the departments and even create new departments."
-    ];
-    
-    const webDestinations = [
-        "https://mvcampbell3.github.io/RPS-Multiplayer/",
-        "https://mvcampbell3.github.io/Word-Guess-Game/",
-        "https://mvcampbell3.github.io/unit-4-game/"
-    ]
-    
-    const gitDestinations = [
-        "https://github.com/Mvcampbell3/RPS-Multiplayer",
-        "https://github.com/Mvcampbell3/Yahtzee",
-        "https://github.com/Mvcampbell3/Word-Guess-Game",
-        "https://github.com/Mvcampbell3/unit-4-game",
-        "https://github.com/Mvcampbell3/liri-node-app"
-    ]
-    
-    $(".projectBox").on("click", function() {
+
+    $(".projectBox").on("click", function () {
         $(".projectBox").removeClass("active");
         $(this).addClass("active");
         let whichProject = parseInt($(this).attr("data-which"));
-        let web = $(this).attr("data-web");
         $("#displayPic").attr("src", $(this).attr("data-carry"));
         $("#projectName").text(nameArray[whichProject]);
         $("#projectContent").text(informationArray[whichProject]);
+        setButtons($(this).attr("data-which"), $(this).attr("data-web"));
     })
 
     const hiddenArray = [].slice.call(document.querySelectorAll(".hidden"));
@@ -95,5 +69,68 @@ function seperately(name) {
             letters[pos].classList.add("fall");
             pos++;
         }
+    }
+}
+
+const nameArray = [
+    "Rock Paper Scissors",
+    "Yahtzee",
+    "Stardew Match Maker",
+    "Burger App",
+    "Bamazon"
+]
+
+const informationArray = [
+    "This is the classic Rock Paper Scissors Game. This is a multiplay game which utilizes the firebase authentication and database technologies. Simply create a user and join in on the 2 player fun.",
+    "This is an ongoing personal project which has its beginnings back when I just started to learn web design. It started as a simple way to learn arrays and is turning into a fully finished game.",
+    "This express app is based on the video game Stardew Valley. You take a survey to see which player from the game you should marry. Since there is online multiplayer, you will also be tested against people who have already taken it. Good Luck!",
+    "This is an express app that utilizes both express-handlebars and MySql to store burgers into a menu. You can add burgers, devour them, put them back, or delete them. Sound tasty?",
+    "Bamazon is a command line node.js app that simulates the interaction on online shopping. Customers can purchase items, Managers can purchase more inventory or add new products. Supervisors can view the financial standings of the departments and even create new departments."
+];
+
+const webDestinations = [
+    "https://mvcampbell3.github.io/RPS-Multiplayer/",
+    "https://secret-temple-27121.herokuapp.com/",
+    "https://burger-app-mvcamp.herokuapp.com/"
+]
+
+const gitDestinations = [
+    "https://github.com/Mvcampbell3/RPS-Multiplayer",
+    "https://github.com/Mvcampbell3/Yahtzee",
+    "https://github.com/Mvcampbell3/friend_finder",
+    "https://github.com/Mvcampbell3/burgers",
+    "https://github.com/Mvcampbell3/MySQL-Node.js"
+]
+
+function setButtons(which, web) {
+    switch (web) {
+        case "true":
+            $("#web").css({ display: "inline-block" });
+            break;
+        case "false":
+            $("#web").css({ display: "none" });
+            break;
+        default:
+            console.log("web switch not working");
+    }
+
+    switch (which) {
+        case "0":
+            $("#web").attr("href", webDestinations[0]);
+            $("#git").attr("href", gitDestinations[0]);
+            break;
+        case "1":
+            $("#git").attr("href", gitDestinations[1]);
+            break;
+        case "2":
+            $("#web").attr("href", webDestinations[1]);
+            $("#git").attr("href", gitDestinations[2]);
+            break;
+        case "3":
+            $("#web").attr("href", webDestinations[2]);
+            $("#git").attr("href", gitDestinations[3]);
+            break;
+        case "4":
+            $("#git").attr("href", gitDestinations[4]);
     }
 }
